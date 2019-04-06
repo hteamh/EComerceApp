2c54e191a24544d7af1603628bfd8d55361dcaf2
hesham <heshamelhakem2020@gmail.com>
2019-04-06 13:38:45 +0200

    hesham

--- app/src/main/java/com/example/a2019/ecomerceapp/Add_Commodity.java
+++ app/src/main/java/com/example/a2019/ecomerceapp/Add_Commodity.java
@@ -9,5 +9,6 @@ public class Add_Commodity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__commodity);
+        jjjjj
     }
 }
