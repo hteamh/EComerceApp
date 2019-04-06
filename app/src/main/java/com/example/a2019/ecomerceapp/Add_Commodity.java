4dc17b77074c83aca15bd0404fc65bc3aa494ccc
hesham <heshamelhakem2020@gmail.com>
2019-04-06 13:40:24 +0200

    hesham

--- app/src/main/java/com/example/a2019/ecomerceapp/Add_Commodity.java
+++ app/src/main/java/com/example/a2019/ecomerceapp/Add_Commodity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Add_Commodity extends AppCompatActivity {
+    int a = 1; // no thing just to commit
+    // delete it
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
