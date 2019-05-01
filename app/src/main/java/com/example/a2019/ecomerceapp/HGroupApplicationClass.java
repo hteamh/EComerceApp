01b42ac89de97d64efa5fff25a8bb9b252500365
hesham <heshamelhakem2020@gmail.com>
2019-05-01 16:18:32 +0300

    1q

--- app/src/main/java/com/example/a2019/ecomerceapp/HGroupApplicationClass.java
+++ app/src/main/java/com/example/a2019/ecomerceapp/HGroupApplicationClass.java
@@ -9,6 +9,6 @@ public class HGroupApplicationClass extends Application  {
     public void onCreate() {
         super.onCreate();
         MyDatabase.init(this);
-        //.........
+        //.........nnn
     }
 }
